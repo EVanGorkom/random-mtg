d658b5f5b41fd3d8f322a907277f81289b160d01
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-05-02 02:34:54 -0500

    Modify Card and Commander endpoints and components to allow for dynamic filtering by color

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,16 +1,37 @@
 import { useState } from 'react';
+import CardFilter from './CardFilter';
 import './Card.css';
 
 function Cards() {
     const [cardImageUrl, setCardImageUrl] = useState('');
-
+    const [selectedColors, setSelectedColors] = useState([]);
     const fetchRandomCard = async () => {
-    try {
-        const response = await fetch('https://api.scryfall.com/cards/random');
-        const data = await response.json();
-        setCardImageUrl(data.image_uris.normal);
+        let uri;
+        if (selectedColors.length > 0) {
+            let colorQuery = `c:${selectedColors.join('')}`;
+            uri = `https://api.scryfall.com/cards/search?q=${colorQuery}&order=random&unique=cards&include_extras=false&include_multilingual=false&include_variations=false`;
+        } else {
+            uri = 'https://api.scryfall.com/cards/random';
+        }
+
+        try {
+            const response = await fetch(uri);
+            const data = await response.json();
+            if (data.data && data.data.length) {
+                // first conditional handles the search endpoint with the color filter. Now we need to randomize and select the card.
+                const randomIndex = Math.floor(Math.random() * data.data.length);
+                const card = data.data[randomIndex];
+                setCardImageUrl(card.image_uris ? card.image_uris.normal : '');
+            } else if (data.image_uris) {
+                // second conditional handles the singular endpoint 'cards/random' which will only ever give a single card
+                setCardImageUrl(data.image_uris ? data.image_uris.normal : '');
+            } else {
+                // third conditional handles errors.
+                setCardImageUrl('');
+                alert('No cards found for the selected colors.');
+            }
         } catch (error) {
-        console.error('Error fetching random card:', error);
+            console.error('Error fetching cards:', error);
         }
     };
 
@@ -23,9 +44,13 @@ function Cards() {
             </div>
         )}
         <br/>
+        Filter by color
+        <CardFilter onColorSelect={setSelectedColors} colors={selectedColors} />
+        <br/>
         <button className="button" onClick={fetchRandomCard}>
-            {cardImageUrl ? 'This card sucks! Find me a new one!' : 'Get a random card'}
+            {cardImageUrl ? 'Find me a new one' : 'Get a random card'}
         </button>
+        <br/>
         </div>
     );
 }
--- src/components/Commander.js
+++ src/components/Commander.js
@@ -1,16 +1,31 @@
 import { useState } from 'react';
+import CardFilter from './CardFilter';
 import './Card.css';
 
 function Commander() {
     const [cardImageUrl, setCardImageUrl] = useState('');
+    const [selectedColors, setSelectedColors] = useState([]);
 
     const fetchRandomCard = async () => {
-    try {
-        const response = await fetch('https://api.scryfall.com/cards/random?q=is%3Acommander');
-        const data = await response.json();
-        setCardImageUrl(data.image_uris.normal);
+        let uri;
+        if (selectedColors.length > 0) {
+            const colorIdentityQuery = selectedColors.join('');
+            uri = `https://api.scryfall.com/cards/random?q=is%3Acommander+color%3D${colorIdentityQuery}`;
+        } else {
+            uri = 'https://api.scryfall.com/cards/random?q=is%3Acommander'
+        }
+
+        try {
+            const response = await fetch(uri);
+            const data = await response.json();
+            if (data.image_uris && data.image_uris.normal) {
+                setCardImageUrl(data.image_uris.normal);
+            } else {
+                setCardImageUrl('');
+                alert('No commander found for the selected colors.');
+            }
         } catch (error) {
-        console.error('Error fetching random card:', error);
+            console.error('Error fetching random card:', error);
         }
     };
 
@@ -23,9 +38,13 @@ function Commander() {
             </div>
         )}
         <br/>
+        Filter by color
+        <CardFilter onColorSelect={setSelectedColors} colors={selectedColors} />
+        <br/>
         <button className="button" onClick={fetchRandomCard}>
             {cardImageUrl ? 'Eh, I\'m not feeling this one' : 'Give me a random Commander'}
         </button>
+        <br/>
         </div>
     );
 }
